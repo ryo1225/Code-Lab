d73afd516a3d8ed5df908cdd762b875068455700
ryo <xxxyyz3332211@gmail.com>
2020-04-22 09:33:30 +0000

    [Add]いいねの非同期通信close #35

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,22 +4,12 @@ class FavoritesController < ApplicationController
     @lab = Lab.find(params[:lab_id])
     favorite = @lab.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
      @lab = Lab.find(params[:lab_id])
      favorite = current_user.favorites.find_by(lab_id: @lab.id)
      favorite.destroy
-     redirect_to request.referer
   end
-    private
-    def redirect
-      case params[:redirect_id].to_i
-    	when 0
-      	redirect_to labs_path
-    	when 1
-      	redirect_to lab_path(@lab)
-      end
-  	end
+
 end
